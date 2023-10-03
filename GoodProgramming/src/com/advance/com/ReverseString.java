e514b4fcc7c533a56a87a51794819d9ab320e17c
saurabh <piyush100489@gmail.com>
2023-10-03 15:40:17 +0530

    commit

---
+++ GoodProgramming/src/com/advance/com/ReverseString.java
@@ -0,0 +1,13 @@
+package com.advance.com;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		String str="PURIMA";
+	    for(int i=str.length()-1;i>=0;i--) {
+	    	System.out.print(str.charAt(i));
+	    }
+
+	}
+
+}
---
+++ GoodProgramming/src/com/advance/com/Ten.java
@@ -0,0 +1,12 @@
+package com.advance.com;
+
+public class Ten {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=10;i++) {
+			System.out.print(" "+i);
+		}
+
+	}
+
+}
